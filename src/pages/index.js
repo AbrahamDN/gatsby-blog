0fc959987694fbbd4a73499fa6eb21d37b903831
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-09-09 17:40:21 +0100

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,76 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const BlogPreview = styled.div`
+  border-radius: 10px;
+  background-color: #e9e2f6;
+  margin: 1em 0;
+  color: rgb(0 0 0 / 0.75);
+  transition: 0.45s ease-in-out;
+
+  &:hover {
+    background-color: #cfc0ea;
+  }
+`
+const BlogLink = styled(Link)`
+  color: inherit;
+  text-decoration: none;
+
+  & > div {
+    padding: 5% 2.5% 2.5%;
+  }
+`
+
+const IndexPage = ({ data }) => {
+  const dataMd = data.allMarkdownRemark
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        <h1>My thoughts</h1>
+        <h4>{dataMd.totalCount}</h4>
+        {dataMd.edges.map(({ node }) => (
+          <BlogPreview key={node.id}>
+            <BlogLink to={node.fields.slug}>
+              <div>
+                <h3>
+                  {node.frontmatter.title} - {node.frontmatter.date}
+                </h3>
+                <p>{node.excerpt}</p>
+              </div>
+            </BlogLink>
+          </BlogPreview>
+        ))}
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            description
+            date
+          }
+          html
+          excerpt
+          fields {
+            slug
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`
---
+++ src/pages/page-3.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const Page3 = () => {
+  return (
+    <Layout>
+      <h1>Hello from the 3rd page!</h1>
+    </Layout>
+  )
+}
+
+export default Page3
